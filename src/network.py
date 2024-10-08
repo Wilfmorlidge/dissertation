fbfa37719bf7e4cb1cbdb0507396a6924aea7415
Wilfmorlidge <wilfmorlidge@gmail.com>
2024-10-08 08:25:55 +0000

    make accuracy metric

--- src/network.py
+++ src/network.py
@@ -12,10 +12,11 @@ def normalize_database(unnormalised_database,length,info='info not provided'):
     print(info)
 
     database = tfds.as_numpy(unnormalised_database.take(length))
-    normalized_database = []
+    normalized_database = {'images': [], 'classifications': []}
     for entry in database:
-        normalized_database.append(resize_image(entry['image']))
-    return np.array(normalized_database)
+        normalized_database['images'].append(resize_image(entry['image']))
+        normalized_database['classifications'].append(entry['label'])
+    return normalized_database
 
 
 def run_prediction(database,model_string = 'resnet'):
@@ -26,14 +27,20 @@ def run_prediction(database,model_string = 'resnet'):
 
     # update to provide human readbale information, e.i what each image was and what it was classified as +
     # percentage of correct classifications, as well as the actual loss function value.
-    scores = model.predict(database)
+    scores = model.predict(np.array(database['images']))
     confidences = []
     classes = []
-    for image in scores:
-        confidences.append(np.max(image))
-        classes.append(np.argmax(image))
+    accuracy = 0
+    for counter in range(0,len(scores)):
+        confidences.append(np.max(scores[counter]))
+        this_class = np.argmax(scores[counter])
+        if this_class == database['classifications'][counter]:
+            accuracy += 1
+        classes.append(this_class)
+    accuracy = accuracy / len(scores)
     dictionary = {'confidences': np.array(confidences),
                   'classes': np.array(classes),
+                  'accuracy': accuracy
     }
     return dictionary
 
