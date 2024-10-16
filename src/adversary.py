f34f50995258cb2d57b37bfff37960f78ec386b2
Wilfmorlidge <wilfmorlidge@gmail.com>
2024-10-16 14:54:43 +0000

    draft of deepfool stage 1

--- src/adversary.py
+++ src/adversary.py
@@ -9,20 +9,38 @@ def find_logit_derivative_value(image,logit,model):
         #all calculations occuring in here are recorded by gradient tape, which allows the gradient of a valeu calculated in this indent to be automatically calculated
         #with respect to a value passed into this indent.
         scores = model(image)
-        retrieved_logit = scores[:1,(logit-1):logit]
-    return watcher.gradient(retrieved_logit,image)
+        retrieved_logit = scores[:1,logit]
+    return np.array(watcher.gradient(retrieved_logit,image))
 
 class AdversarialAttacks:
     def DeepFool_iteration_step(self,image,classification,model):
-        image = np.expand_dims(image, axis=0)
-        scores = model(image)
-        print('step 1: identifying closest boundary')
-        print('step 1a: calculating absolute distance in logits')
+        image1 = np.expand_dims(image, axis=0)
+        scores = model(image1)
+        logit_derivative_for_true_class = find_logit_derivative_value(image,classification,model)
+        print('step 1: identifying closest boundary via heuristic')
+        #this iterates though all possible classes for each image
+        minimum_absolute_boundary_distance = 999999999999999999999999999
+        minimum_euclidean_distance = 9999999999999999999999
+        minimum_heuristic = 99999999999999999999
+        minimum_logit_derivative = 9999999999999999999
         for counter in range(0,999):
-            #print('checking class boundary:' + str(counter))
             if counter != classification:
-                current_absolute_boundary_distance = abs(scores[:1,(counter-1):counter] - scores[:1,(classification-1):classification])
+                #this calculates the absolute distance between the class to be checked and the true class
+                current_absolute_boundary_distance = np.array(abs(scores[:1,counter] - scores[:1,(classification-1):classification]))[0,0]
                 print('current absolute boundary distance:' + str(current_absolute_boundary_distance))
+                #this calculates the euclidean distance between the derivatives of the logits for those classes
+                logit_derivative_for_class_being_checked = find_logit_derivative_value(image,counter,model)
+                current_euclidean_distance = np.linalg.norm(logit_derivative_for_class_being_checked - logit_derivative_for_true_class)
+                print('euclidean distance for current class' + str(current_euclidean_distance))
+                if current_absolute_boundary_distance/current_euclidean_distance < minimum_heuristic:
+                    minimum_absolute_boundary_distance = current_absolute_boundary_distance
+                    minimum_euclidean_distance = current_euclidean_distance
+                    minimum_logit_derivative = logit_derivative_for_class_being_checked
+        print('minimum absolute boundary_distance' + str(minimum_absolute_boundary_distance))
+        print('minimum euclidean distance' + str(minimum_euclidean_distance))
+        print('minimum logit derivative' + str(minimum_logit_derivative))
+        print('step 2: calculate pertubation')
+
                 
 
         return np.zeros(image.shape)
