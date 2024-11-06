2e1e70b87718da42b8f4b761ea00be68d58872c8
Wilfmorlidge <wilfmorlidge@gmail.com>
2024-11-06 11:46:27 +0000

    begin fixing rounding errors

--- src/adversary.py
+++ src/adversary.py
@@ -15,43 +15,53 @@ def find_logit_derivative_value(image,logit,model):
 
 class AdversarialAttacks:
     def DeepFool_iteration_step(self,image,classification,model):
+        print('perhaps i am an idiot' + str(50-1e-10))
+        image = image.astype(np.float64)
         class_list = [0,217,482,491,497,566,569,571,574,701]
-        print('this is the shape of the original image' + str(np.array(image).shape))
+        #print('this is the shape of the original image' + str(np.array(image).shape))
         print('this is the original image' +str(np.array(image)))
         image1 = np.expand_dims(image, axis=0)
         scores = model(image1)
-        logit_derivative_for_true_class = find_logit_derivative_value(image,classification,model)
-        print('step 1: identifying closest boundary via heuristic')
-        #this iterates though all possible classes for each image
-        minimum_absolute_boundary_distance = 999999999999999999999999999
-        minimum_euclidean_distance = 9999999999999999999999
-        minimum_heuristic = 99999999999999999999
-        minimum_logit_derivative = 9999999999999999999
-        nearest_class = 0
-        for entry in class_list:
-            print('reviewing class:' + str(entry))
-            if entry != classification:
-                #this calculates the absolute distance between the class to be checked and the true class
-                current_absolute_boundary_distance = np.array(abs(scores[:1,entry] - scores[:1,(classification-1):classification]))[0,0]
-                #this calculates the euclidean distance between the derivatives of the logits for those classes
-                logit_derivative_for_class_being_checked = find_logit_derivative_value(image,entry,model)
-                current_euclidean_distance = np.linalg.norm(logit_derivative_for_class_being_checked - logit_derivative_for_true_class)
-                if current_absolute_boundary_distance/current_euclidean_distance < minimum_heuristic:
-                    minimum_absolute_boundary_distance = current_absolute_boundary_distance
-                    minimum_euclidean_distance = current_euclidean_distance
-                    minimum_logit_derivative = logit_derivative_for_class_being_checked
-                    nearest_class = entry
-        print('minimum absolute boundary_distance ' + str(minimum_absolute_boundary_distance))
-        print('minimum euclidean distance ' + str(minimum_euclidean_distance))
-        print('minimum logit derivative ' + str(minimum_logit_derivative))
-        print('nearest class ' + str(nearest_class))
-        print('step 2: calculate pertubation')
-        #this component finds the pertubation to be applied to the image
-        pertubation = ((minimum_absolute_boundary_distance) / (minimum_euclidean_distance ** 2)) * (minimum_logit_derivative-logit_derivative_for_true_class)
-        print('pertubation:' + str(pertubation))
-        print('this is the shape of the pertubation' + str(np.squeeze(np.array(pertubation),axis=0).shape))
-        image = image + np.squeeze(np.array(pertubation),axis=0)
-        print('this is the shape of the pertubed image' + str(np.array(image).shape))
+        loop_counter = 0
+        while (np.argmax(scores) == classification) and (loop_counter < 20):
+            loop_counter += 1
+            print('now entering pertubation cycle ' + str(loop_counter))
+            logit_derivative_for_true_class = find_logit_derivative_value(image,classification,model)
+            print('step 1: identifying closest boundary via heuristic')
+            #this iterates though all possible classes for each image
+            minimum_absolute_boundary_distance = 999999999999999999999999999
+            minimum_euclidean_distance = 9999999999999999999999
+            minimum_heuristic = 99999999999999999999
+            minimum_logit_derivative = 9999999999999999999
+            nearest_class = 0
+            for entry in class_list:
+                print('reviewing class:' + str(entry))
+                if entry != classification:
+                    #this calculates the absolute distance between the class to be checked and the true class
+                    current_absolute_boundary_distance = np.array(abs(scores[:1,entry] - scores[:1,(classification-1):classification]),dtype=np.float64)[0,0]
+                    #this calculates the euclidean distance between the derivatives of the logits for those classes
+                    logit_derivative_for_class_being_checked = find_logit_derivative_value(image,entry,model)
+                    current_euclidean_distance = np.linalg.norm(logit_derivative_for_class_being_checked - logit_derivative_for_true_class)
+                    if current_absolute_boundary_distance/current_euclidean_distance < minimum_heuristic:
+                        minimum_absolute_boundary_distance = current_absolute_boundary_distance
+                        minimum_euclidean_distance = current_euclidean_distance
+                        minimum_logit_derivative = logit_derivative_for_class_being_checked
+                        nearest_class = entry
+            print('minimum absolute boundary_distance ' + str(minimum_absolute_boundary_distance))
+            print('minimum euclidean distance ' + str(minimum_euclidean_distance))
+            #print('minimum logit derivative ' + str(minimum_logit_derivative))
+            print('nearest class ' + str(nearest_class))
+            print('step 2: calculate pertubation')
+            #this component finds the pertubation to be applied to the image
+            pertubation = ((minimum_absolute_boundary_distance) / (minimum_euclidean_distance ** 2)) * (minimum_logit_derivative-logit_derivative_for_true_class)
+            print('pertubation:' + str(pertubation))
+            #print('this is the shape of the pertubation' + str(np.squeeze(np.array(pertubation),axis=0).shape))
+            image = image + np.squeeze(np.array(pertubation, dtype = np.longdouble),axis=0)
+            print('am i an idiot?'+ str(np.squeeze(np.array(pertubation, dtype = np.longdouble),axis=0)))
+            #print('this is the shape of the pertubed image' + str(np.array(image).shape))
+            print('this is the pertubed image' + str(image))
+            image1 = np.expand_dims(image, axis=0)
+            scores = model(image1)
                 
 
         return image
