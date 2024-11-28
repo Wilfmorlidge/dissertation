df2c4b4e52caa79293dff66f1e20ee6407d796e2
Wilfmorlidge <wilfmorlidge@gmail.com>
2024-11-28 10:34:43 +0000

    create application window

--- src/window.py
+++ src/window.py
@@ -7,9 +7,26 @@ def denormalize_and_save_image(image,ident,type):
     display_2.save(f'./images/{type}/image_{ident}.png')
 
 from tkinter import ttk
+
+def define_window(window):
+    #this component defines the aesthetic attributes of the window and the application background
+    window.geometry("1500x750")
+    window.resizable(False, False)
+    window.attributes('-topmost', 1)
+    window.configure(bg='dimgray')
+    window.update_idletasks()
+    width = window.winfo_width()
+    height = window.winfo_height()
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+    x = (screen_width - width) // 2
+    y = (screen_height - height) // 2
+    window.geometry(f"{width}x{height}+{x}+{y}")
+
 def front_end_main():
     root = tk.Tk()
-
-    tk.Label(root, text='Classic Label').pack()
-    ttk.Label(root, text='Themed Label').pack()
+    define_window(root)
     root.mainloop()
+
+
+test = front_end_main()
