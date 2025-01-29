912a2c4046c7a5c4dd501200036cc466f627e04b
Wilfmorlidge <wilfmorlidge@gmail.com>
2025-01-29 14:38:28 +0000

    add support for calculating the absolute locationj of imagesin a list

--- src/GUI/objects/scroll_list/scroll_list_entries/image_display_scroll_list_entries.py
+++ src/GUI/objects/scroll_list/scroll_list_entries/image_display_scroll_list_entries.py
@@ -1,84 +1,111 @@
 import os
 import tkinter as tk
 from PIL import Image, ImageTk
+import time
 
 def update_image_display_entries(root,display_width,entry_height,dictionary, variable, display_height):
-    while not dictionary.empty():
-        current_trial = dictionary.get()
-        file_count = 0
-        for entry in os.listdir( f"./results/trial_{current_trial}/pertubed"):
-            # Check if the entry is a file
-            if os.path.isfile(os.path.join(f"./results/trial_{current_trial}/pertubed", entry)):
-                file_count += 1
-
-        for counter in range(0,file_count):
-            container_container = tk.Frame(root,bg='dimgray')
 
 
-            listing_container = tk.Frame(container_container, height = int(entry_height * 1/5))
-            listing = tk.Label(listing_container,text=f"{current_trial}:{counter}")
+    def create_new_entries(root,display_width,entry_height,dictionary, variable, display_height):
+        while not dictionary.empty():
+            current_trial = dictionary.get()
+            file_count = 0
+            for entry in os.listdir( f"./results/trial_{current_trial}/pertubed"):
+                # Check if the entry is a file
+                if os.path.isfile(os.path.join(f"./results/trial_{current_trial}/pertubed", entry)):
+                    file_count += 1
 
-            image_container = tk.Frame(container_container, height = int(entry_height * 4/5))
+            for counter in range(0,file_count):
+                container_container = tk.Frame(root,bg='dimgray')
 
-            unpertubed_image = Image.open( f"./results/trial_{current_trial}/unpertubed/image_{counter}.png")
-            unpertubed_image = unpertubed_image.resize((int(display_width/3) -30,int(entry_height * 4/5)-30))
-            unpertubed_image = ImageTk.PhotoImage(unpertubed_image)
-            unpertubed_label = tk.Label(image_container,image = unpertubed_image)
-            unpertubed_label.image = unpertubed_image
 
-            pertubation_image = Image.open( f"./results/trial_{current_trial}/pertubation/image_{counter}.png")
-            pertubation_image = pertubation_image.resize((int(display_width/3) -30,int(entry_height * 4/5)-30))
-            pertubation_image = ImageTk.PhotoImage(pertubation_image)
-            pertubation_label = tk.Label(image_container,image = pertubation_image)
-            pertubation_label.image = pertubation_image
+                listing_container = tk.Frame(container_container, height = int(entry_height * 1/5))
+                listing = tk.Label(listing_container,text=f"{current_trial}:{counter}")
 
-            pertubed_image = Image.open( f"./results/trial_{current_trial}/pertubed/image_{counter}.png")
-            pertubed_image = pertubed_image.resize((int(display_width/3)-30,int(entry_height * 4/5)-30))
-            pertubed_image = ImageTk.PhotoImage(pertubed_image)
-            pertubed_label = tk.Label(image_container,image = pertubed_image)
-            pertubed_label.image = pertubed_image
+                image_container = tk.Frame(container_container, height = int(entry_height * 4/5))
 
-            listing.pack(side='left')
-            listing_container.pack(side='top',fill='x', expand=(True))
+                unpertubed_image = Image.open( f"./results/trial_{current_trial}/unpertubed/image_{counter}.png")
+                unpertubed_image = unpertubed_image.resize((int(display_width/3) -30,int(entry_height * 4/5)-30))
+                unpertubed_image = ImageTk.PhotoImage(unpertubed_image)
+                unpertubed_label = tk.Label(image_container,image = unpertubed_image)
+                unpertubed_label.image = unpertubed_image
+
+                pertubation_image = Image.open( f"./results/trial_{current_trial}/pertubation/image_{counter}.png")
+                pertubation_image = pertubation_image.resize((int(display_width/3) -30,int(entry_height * 4/5)-30))
+                pertubation_image = ImageTk.PhotoImage(pertubation_image)
+                pertubation_label = tk.Label(image_container,image = pertubation_image)
+                pertubation_label.image = pertubation_image
+
+                pertubed_image = Image.open( f"./results/trial_{current_trial}/pertubed/image_{counter}.png")
+                pertubed_image = pertubed_image.resize((int(display_width/3)-30,int(entry_height * 4/5)-30))
+                pertubed_image = ImageTk.PhotoImage(pertubed_image)
+                pertubed_label = tk.Label(image_container,image = pertubed_image)
+                pertubed_label.image = pertubed_image
+
+                listing.pack(side='left')
+                listing_container.pack(side='top',fill='x', expand=(True))
            
-            unpertubed_label.pack(side = 'left',padx=(10,0),pady=(22.5,22.5))
-            pertubation_label.pack(side = 'left',padx=(5,5),pady=(22.5,22.5))
-            pertubed_label.pack(side = 'left',padx=(0,10),pady=(22.5,22.5))
-            image_container.pack(side = 'top')
-            container_container.pack(side='top',expand=True,fill='x',pady=(0,30),padx=(22.5,22.5))
+                unpertubed_label.pack(side = 'left',padx=(10,0),pady=(22.5,22.5))
+                pertubation_label.pack(side = 'left',padx=(5,5),pady=(22.5,22.5))
+                pertubed_label.pack(side = 'left',padx=(0,10),pady=(22.5,22.5))
+                image_container.pack(side = 'top')
+                container_container.pack(side='top',expand=True,fill='x',pady=(0,30),padx=(22.5,22.5))
+                variable[2].append(container_container)
+        root.after(500, lambda: create_new_entries(root,display_width,entry_height,dictionary, variable,display_height))
 
 
-    print(root.winfo_children())
-    for widget in root.winfo_children():
-        canvas_width = variable.winfo_width() 
-        canvas_height = variable.winfo_height() 
-        x_scroll = variable.canvasx(0) 
-        y_scroll = variable.canvasy(0) # Calculate the visible region 
-        visible_region = (x_scroll, y_scroll, x_scroll + canvas_width, y_scroll + canvas_height)
+
+    def lazy_load_entries(root,display_width,entry_height,dictionary, variable, display_height,top_frame,bottom_frame):
+
+        for widget in variable[2]:
+           if not(widget.winfo_name() == 'exception_frame'):
+                # we will need to create an ordered list of widgets so that things are still packed in the correct order
+                try:
+                    widget.pack(side='top',expand=True,fill='x',pady=(0,30),padx=(22.5,22.5))
+
+                    root.update_idletasks()
+                    root.update()
+                except:
+                    nothing = None
 
 
-        x = widget.winfo_x() 
-        y = widget.winfo_y() 
-        width = widget.winfo_width()
-        height = widget.winfo_height()
-        widget_bbox = (x, y, x + width, y + height)
+        current_widget_height = (variable[0].bbox("all")[1]) -137
+        top_padding = 0
+        bottom_padding = 0
+        top_frame = tk.Frame(root,name='top_frame',bg='green')
+        entries_to_be_rendered = [top_frame]
 
-        print(widget_bbox)
+        canvas_width = variable[0].winfo_width() 
+        canvas_height = variable[0].winfo_height() 
+        x_scroll = variable[0].canvasx(0) 
+        y_scroll = variable[0].canvasy(0) # Calculate the visible region 
+        visible_region = [x_scroll, y_scroll -150, x_scroll + canvas_width, y_scroll + canvas_height + 150]
+
         print(visible_region)
+        print('top of canvas height' + str(current_widget_height))
 
-        #    widget_height = widget.winfo_height() + widget.pack_info().get('pady', 0)
-        #    if (widget_bbox[2] > canvas_bbox[0] and widget_bbox[0] < canvas_bbox[2] and widget_bbox[3] > canvas_bbox[1] and widget_bbox[1] < canvas_bbox[3]):
-        #        root.pack_configure(pady = root.pack_info().get('pady', 0) - widget_height)
-        #        try:
-        #            widget.pack(side='top',expand=True,fill='x',pady=(0,30),padx=(22.5,22.5))
-        #        except:
-        #            nothing = None
-        #    else:
-        #        root.pack_configure(pady = root.pack_info().get('pady', 0) + widget_height)
-        #        try:
-        #            widget.pack_forget()
-        #        except:
-        #            nothing = None
+        for widget in variable[2]:
+            print(variable[2])
+            print('beginning run')
+            if not(widget.winfo_name() == 'exception_frame') and not(widget.winfo_name() == 'top_frame') and not(widget.winfo_name() == 'bottom_frame'):
+                print('accessing element')
 
-        
-    root.after(500, lambda: update_image_display_entries(root,display_width,entry_height,dictionary, variable,display_height))
+                if (current_widget_height + entry_height) < visible_region[1]:
+                    print('this element is too high')
+                    top_padding += entry_height + 30
+                    current_widget_height += entry_height + 30
+                elif current_widget_height > visible_region[3]:
+                    print('this element is too low')
+                    bottom_padding += entry_height + 30
+                    current_widget_height += entry_height + 30
+                else:
+                    entries_to_be_rendered.append(widget)
+                    current_widget_height += entry_height + 30
+            
+
+
+    top_frame = tk.Frame(root,name='top_frame',bg='green')
+    bottom_frame = tk.Frame(root,name='bottom_frame',bg='blue')
+
+    variable[1].bind("<ButtonRelease-1>", lambda event: lazy_load_entries(root,display_width,entry_height,dictionary, variable,display_height))
+    root.after(500, lambda: create_new_entries(root,display_width,entry_height,dictionary, variable,display_height))
