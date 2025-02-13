d110aef32609089408772cb98f25d6e5f0e56606
Wilfmorlidge <wilfmorlidge@gmail.com>
2025-02-13 11:52:42 +0000

    reduce the number of decimal places to which cumulaative metrics are displayed on screen

--- src/GUI/objects/result_display/result_display.py
+++ src/GUI/objects/result_display/result_display.py
@@ -20,28 +20,28 @@ def result_display(root,display_height, display_width):
         accuracy_container = tk.Frame(object_container,bg='dimgray')
 
         accuracy_label = tk.Label(accuracy_container, text='accuracy',bg='dimgray',font=('helvetica',20))
-        accuracy_display = tk.Label(accuracy_container,text=str(current_cumulative_values['accuracy']),bg='dimgray',fg='white',font=('helvetica',20))
+        accuracy_display = tk.Label(accuracy_container,text=f"{current_cumulative_values['accuracy']:.{3}f}",bg='dimgray',fg='white',font=('helvetica',20))
 
         accuracy_label.pack(side='top',pady=(0,25))
         accuracy_display.pack(side='top')
 
         mean_pertubation_container = tk.Frame(object_container,bg='dimgray')
         mean_pertubation_label = tk.Label(mean_pertubation_container,text='mean pertubation',bg='dimgray',font=('helvetica',20))
-        mean_pertubation_display = tk.Label(mean_pertubation_container,text=str(current_cumulative_values['mean_pertubation']),bg='dimgray',fg='white',font=('helvetica',20))
+        mean_pertubation_display = tk.Label(mean_pertubation_container,text=f"{current_cumulative_values['mean_pertubation']:.{3}f}",bg='dimgray',fg='white',font=('helvetica',20))
 
         mean_pertubation_label.pack(side='top',pady=(0,25))
         mean_pertubation_display.pack(side='top')
 
         GMQ_container = tk.Frame(object_container,bg='dimgray')
         GMQ_label = tk.Label(GMQ_container,text='GMQ',bg='dimgray',font=('helvetica',20))
-        GMQ_display = tk.Label(GMQ_container,text=str(current_cumulative_values['GMQ']),bg='dimgray',fg='white',font=('helvetica',20))
+        GMQ_display = tk.Label(GMQ_container,text=f"{current_cumulative_values['GMQ']:.{3}f}",bg='dimgray',fg='white',font=('helvetica',20))
 
         GMQ_label.pack(side='top',pady=(0,25))
         GMQ_display.pack(side='top')
 
         Sharpe_ratio_container = tk.Frame(object_container,bg='dimgray')
         Sharpe_ratio_label = tk.Label(Sharpe_ratio_container,text='Sharpe ratio',bg='dimgray',font=('helvetica',20))
-        Sharpe_ratio_display = tk.Label(Sharpe_ratio_container,text=str(current_cumulative_values['Sharpe_ratio']),bg='dimgray',fg='white',font=('helvetica',20))
+        Sharpe_ratio_display = tk.Label(Sharpe_ratio_container,text=f"{current_cumulative_values['Sharpe_ratio']:.{3}f}",bg='dimgray',fg='white',font=('helvetica',20))
 
         Sharpe_ratio_label.pack(side='top',pady=(0,25))
         Sharpe_ratio_display.pack(side='top')
